7cd738efdcdb0d5c83ca7fff2301c9171a48ab43
mlietti1 <mcf.lietti@outlook.com>
2022-10-14 20:03:42 +0200

    dadi

--- gioco-dadi/js/script.js
+++ gioco-dadi/js/script.js
@@ -1,14 +1,27 @@
 let numUtente;
 let numEstratto;
+const dadoUser = document.querySelector('.dado-user');
+const dadoRandom = document.querySelector('.dado-random');
+
+const risultato = document.querySelector('.risultato');
 
 const rollDice = document.getElementById('btn');
 
 rollDice.addEventListener('click', function(){
-  
+
   numUtente = document.getElementById('number').value;
-  console.log(numUtente);
+  dadoUser.innerHTML = numUtente;
+  
 
   numEstratto = Math.floor(Math.random() * 6) + 1;
-  console.log(numEstratto);
+  dadoRandom.innerHTML = numEstratto;
+
+  if(numUtente > numEstratto){
+    risultato.innerHTML = `Hai vinto!`
+  }else if(numUtente < numEstratto){
+    risultato.innerHTML = `Ritenta...`
+  }else{
+    risultato.innerHTML = `Pareggio`
+  };
 
 });
