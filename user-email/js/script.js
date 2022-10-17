55ff55c44d65c6e8506a8dc949a1ae67c2e85164
mlietti1 <mcf.lietti@outlook.com>
2022-10-17 09:52:03 +0200

    correzione mail

--- user-email/js/script.js
+++ user-email/js/script.js
@@ -7,24 +7,32 @@ const emailList = [
   'paolo.verdi@mail.com'
 ];
 
-let userEmail;
 const btnCheck = document.getElementById('btn');
+
+let mailPresente = false;
+
 
 
 btnCheck.addEventListener('click', function(){
   
-  userEmail = document.getElementById('email').value;
+  const userEmail = document.getElementById('email').value;
   for (let i = 0; i < emailList.length; i++){
     
     console.log(userEmail);
     console.log(emailList[i]);
 
     if (emailList[i] === userEmail){
+      mailPresente = true;
+    };
+
+    if (mailPresente){
       document.getElementById('output').innerHTML = `You are in the mailing list!`;
     }else{
       document.getElementById('output').innerHTML = `You are <strong>not</strong> in the mailing list.`;
     }
   };
 
-
+  mailPresente = false;
+  document.getElementById('email').value = '';
+  
 });
